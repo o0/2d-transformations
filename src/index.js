871528e60934f4ffb904859185967389d2c3ce04
o0 <igor.alexeenko@htmlacademy.ru>
2019-02-11 05:02:40 +0300

    Adds ESLint

--- src/index.js
+++ src/index.js
@@ -4,55 +4,57 @@ import {Matrix} from 'ml-matrix';
 const degToRad = (deg) => deg * Math.PI / 180;
 
 
-const getRotateMatrix = (deg, translateX = 0, translateY = 0) => new Matrix([
+const getRotateMatrix = (deg, _translateX = 0, _translateY = 0) => new Matrix([
   [Math.cos(degToRad(deg)), Math.sin(degToRad(deg))],
   [-Math.sin(degToRad(deg)), Math.cos(degToRad(deg))],
 ]);
 
 
-const getScaleMatrix = (factor, translateX = 0, translateY = 0) => new Matrix([
+const getScaleMatrix = (factor, _translateX = 0, _translateY = 0) => new Matrix([
   [factor, 0],
   [0, factor],
 ]);
 
 
-const getRotateAndScaleMatrix = (angle, scaleFactor) =>
-    getRotateMatrix(angle).mmul(getScaleMatrix(scaleFactor));
+const getRotateAndScaleMatrix = (angle, scaleFactor) => getRotateMatrix(angle)
+  .mmul(getScaleMatrix(scaleFactor));
 
 
 const renderNextFrame = (shape, ctx, start = null) => {
-  if (Date.now() - start > 300) return;
+  if (Date.now() - start > 300) {
+    return;
+  }
 
   const maxShade = 128;
   const shade = (Date.now() - start) * maxShade / 300;
+  ctx.save();
   ctx.fillStyle = `rgb(0, ${shade}, 0)`;
 
   ctx.beginPath();
   shape.forEach((it, i) => {
-    if (i === 0) ctx.moveTo(it[0], it[1])
-    else ctx.lineTo(it[0], it[1])
+    if (i === 0) {
+      ctx.moveTo(it[0], it[1]);
+    } else {
+      ctx.lineTo(it[0], it[1]);
+    }
   });
   ctx.closePath();
   ctx.fill();
+  ctx.restore();
 
   requestAnimationFrame(
-    renderNextFrame.bind(null, shape.mmul(getRotateAndScaleMatrix(2, 1.01)), ctx, start),
+      renderNextFrame.bind(null, shape.mmul(getRotateAndScaleMatrix(2, 1.01)), ctx, start)
   );
 };
 
 
 const initialize = () => {
-  const container = document.all['plane'];
-  const ctx = container.getContext('2d');
-
-  const shape = new Matrix([
+  renderNextFrame(new Matrix([
     [100, 100],
     [200, 100],
     [200, 200],
     [100, 200],
-  ]);
-
-  renderNextFrame(shape, ctx, Date.now());
+  ]), document.all[`plane`].getContext(`2d`), Date.now());
 };
 
 
